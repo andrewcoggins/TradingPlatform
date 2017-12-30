d9dea3473e194376141f9b8249d039a6ea4502cb
Andrew Coggins <acoggins38@gmail.com>
2017-12-30 14:24:40 -0500

    acct manager test

---
+++ src/test/java/brown/accounting/AccountManagerTest.java
@@ -0,0 +1,40 @@
+package brown.accounting;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+
+import brown.tradeable.library.Tradeable;
+
+public class AccountManagerTest {
+  // not much to this one. Just test the getter and setter.
+  @Test
+  public void testAccountManager() { 
+    AccountManager testManager = new AccountManager();
+    //add some accounts
+    List<Tradeable> oneGoods = new LinkedList<Tradeable>();
+    oneGoods.add(new Tradeable(0)); 
+    oneGoods.add(new Tradeable(1));
+    Account one = new Account(1, 10.0, oneGoods);
+    testManager.setAccount(1, one);
+    List<Tradeable> twoGoods = new LinkedList<Tradeable>();
+    twoGoods.add(new Tradeable(2)); 
+    twoGoods.add(new Tradeable(3));
+    Account two = new Account(1, 20.0, twoGoods);
+    testManager.setAccount(2, two);
+    assertTrue(testManager.containsAcct(1));
+    assertTrue(testManager.containsAcct(2));
+    List<Account> allAccounts = testManager.getAccounts(); 
+    assertTrue(allAccounts.contains(one));
+    assertTrue(allAccounts.contains(two)); 
+    assertTrue(allAccounts.size() == 2); 
+    Account newOne = testManager.getAccount(1); 
+    Account newTwo = testManager.getAccount(2); 
+    assertEquals(newOne, one); 
+    assertEquals(newTwo, two);  
+  }
+}
