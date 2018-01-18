c66868eff802094bde7a947957591a32dfdb939e
Andrew Coggins <acoggins38@gmail.com>
2018-01-18 13:03:02 -0500

    fixed constructing trade requests

--- src/main/java/brown/tradeable/ITradeable.java
+++ src/main/java/brown/tradeable/ITradeable.java
@@ -6,10 +6,6 @@ public interface ITradeable {
   
   public Integer getCount();
   
-<<<<<<< HEAD
-  //TODO: flatten? 
-=======
   public TradeableType getType();
   
->>>>>>> 85b1b158090638b76605d05732a3dd9a1d7fe0ac
 }
