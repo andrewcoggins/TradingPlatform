acf0873daf5458b416cd4b0b7b795075de26ded3
Andrew Coggins <acoggins38@gmail.com>
2018-01-23 11:18:47 -0500

    cleaned up bankupdate

--- src/main/java/brown/messages/library/BankUpdateMessage.java
+++ src/main/java/brown/messages/library/BankUpdateMessage.java
@@ -11,27 +11,32 @@ public class BankUpdateMessage extends AbsMessage {
   
 	public final ITradeable tradeableAdded; 
 	public final ITradeable tradeableLost;
-	public final Double moniesAdded; 
+	public final Double moniesChanged; 
 	
 	public BankUpdateMessage() {
 		super(null);
 		this.tradeableAdded = null; 
 		this.tradeableLost = null;
-		this.moniesAdded = null;
+		this.moniesChanged = null;
 	}
 
-	/**
-	 * Constructor for BankUpdate
-	 * @param ID : agent ID
-	 * @param oldAccount : old account
-	 * @param newAccount : new account; contains info relevant to update
-	 */
+/**
+ * constructor for bank update message
+ * @param ID
+ * private agent id
+ * @param tradeableAdded
+ * tradeables added to agent account
+ * @param tradeableLost
+ * tradeables lost from agent account
+ * @param moniesAdded
+ * monies added to or removed from agent account
+ */
 	public BankUpdateMessage(int ID, ITradeable tradeableAdded, ITradeable tradeableLost, 
-	    double moniesAdded) {
+	    double moniesChanged) {
 		super(ID);
 		this.tradeableAdded = tradeableAdded; 
 		this.tradeableLost = tradeableLost; 
-		this.moniesAdded = moniesAdded; 
+		this.moniesChanged = moniesChanged; 
 	}
 
 	@Override
@@ -39,5 +44,51 @@ public class BankUpdateMessage extends AbsMessage {
 		agent.onBankUpdate(this);
 	}
 
+  @Override
+  public String toString() {
+    return "BankUpdateMessage [tradeableAdded=" + tradeableAdded
+        + ", tradeableLost=" + tradeableLost + ", moniesChanged="
+        + moniesChanged + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result
+        + ((moniesChanged == null) ? 0 : moniesChanged.hashCode());
+    result = prime * result
+        + ((tradeableAdded == null) ? 0 : tradeableAdded.hashCode());
+    result = prime * result
+        + ((tradeableLost == null) ? 0 : tradeableLost.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    BankUpdateMessage other = (BankUpdateMessage) obj;
+    if (moniesChanged == null) {
+      if (other.moniesChanged != null)
+        return false;
+    } else if (!moniesChanged.equals(other.moniesChanged))
+      return false;
+    if (tradeableAdded == null) {
+      if (other.tradeableAdded != null)
+        return false;
+    } else if (!tradeableAdded.equals(other.tradeableAdded))
+      return false;
+    if (tradeableLost == null) {
+      if (other.tradeableLost != null)
+        return false;
+    } else if (!tradeableLost.equals(other.tradeableLost))
+      return false;
+    return true;
+  }
 
 }
