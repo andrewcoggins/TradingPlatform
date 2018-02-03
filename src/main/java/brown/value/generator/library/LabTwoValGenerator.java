7d0d538a0d775d5c6e11d68ce8cb29cbd4ad3b2d
Andrew Coggins <acoggins38@gmail.com>
2018-02-03 12:47:40 -0500

    lab 2 value generator

---
+++ src/main/java/brown/value/generator/library/LabTwoValGenerator.java
@@ -0,0 +1,25 @@
+package brown.value.generator.library;
+
+import brown.value.generator.IValuationGenerator;
+
+/**
+ * value generator for lab two: 
+ * characterized by F(x) = 2x - x^2 with support on [0, 1]
+ * @author andrew
+ *
+ */
+public class LabTwoValGenerator implements IValuationGenerator {
+
+  public LabTwoValGenerator(){ 
+    
+  }
+  
+  @Override
+  public Double makeValuation() {
+    // TODO Auto-generated method stub
+    double y = Math.random();
+    Double value = 1 - Math.sqrt(1 - y);
+    return value;
+  }
+  
+}
