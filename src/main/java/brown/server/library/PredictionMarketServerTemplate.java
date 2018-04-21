08d38fecd227c52df45be650fc9cc74577d72dd4
ksyan <kerryan34@gmail.com>
2018-04-21 14:29:42 -0400

    random

--- src/main/java/brown/server/library/PredictionMarketServerTemplate.java
+++ src/main/java/brown/server/library/PredictionMarketServerTemplate.java
@@ -2,7 +2,7 @@ package brown.server.library; // TODO: change this to your package
 
 public class PredictionMarketServerTemplate {
 	private static int seconds = 30; 	// number of seconds per round
-	private static int nSims = 100; 		// number of rounds to run
+	private static int nSims = 1; 		// number of rounds to run
 	private static int init_delay = 5; 	// time to wait before beginning the simulation
 	private static int lag = 25;			// time between intervals in which bots can trade
   private static String outFile = "Put Your File Here";
