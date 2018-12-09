eac4d03ad3faa05bf64726d5b1f109e6937a3810
AmyGreenwald <amy@brown.edu>
2018-12-10 08:59:32 +0900

    figuring out the Market logic

--- src/main/java/brown/platform/market/library/Market.java
+++ src/main/java/brown/platform/market/library/Market.java
@@ -66,7 +66,11 @@ public class Market implements IMarket {
     return this.STATE.getID();
   }
 
-  
+  // Processing bids is a four step process:
+  // 1. Send trade requests via Query Rule
+  // 2. Check acceptability via Activity Rule
+  // 3. Find allocation and payments (via these rules)
+  // 4. Send game report (via IR policy)
   public TradeRequestMessage constructTradeRequest(Integer ID) {
     // no idea why ledgers are part of the trade request -- they should be sent as market updates!
     this.QRULE.makeChannel(STATE);
@@ -78,20 +82,20 @@ public class Market implements IMarket {
   // name seems misleading
   public boolean handleBid(TradeMessage bid) {
     this.ACTRULE.isAcceptable(this.STATE, bid); 
+    // why are we checking isOpen here? should check this much earlier!
     if (this.STATE.getAcceptable() && this.STATE.isOpen()) {
       STATE.addBid(bid);
     }
     return this.STATE.getAcceptable();
   }
 
-  // this seems more like constructOrders
   public List<Order> constructOrders() {
     // Set allocation and payment
     this.ARULE.setAllocation(this.STATE);
-    // construct orders
     this.PRULE.setOrders(this.STATE); // setPayment
 
     // Construct orders from allocation and payments
+    // why no? return this.STATE.getAllocation();
     return this.STATE.getPayments();
   }
 
@@ -102,18 +106,24 @@ public class Market implements IMarket {
     return this.STATE.getReport();
   }
   
+  
+  // Move to MarketState
+  // For example, this needs to be done in the logic before getAcceptable,
+  // e.g., once per iteration in an Open Outcry auction
   @Override
   public void setReserves() {
     this.ACTRULE.setReserves(this.STATE); 
   }
   
+  // This stays, b/c Bids are being moved out of MarketState
+  // But all that logic might end up somewhere else
   @Override
   public void clearBidCache() {
     this.STATE.clearBids();
   }
   
   
-  
+  // The remaining logic pertains to the Termination Condition
   @Override
   public void tick() {
     this.STATE.tick();
@@ -134,7 +144,6 @@ public class Market implements IMarket {
   @Override
   public void close() {
     this.STATE.close(); 
-  }
-  
+  } 
   
 }
