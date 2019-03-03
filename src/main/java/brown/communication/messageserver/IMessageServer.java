8762f3a50ea2606867336a85bef1b57058a04780
Andrew Coggins <acoggins38@gmail.com>
2019-03-03 09:54:41 -0500

    stashed changes

--- src/main/java/brown/communication/messageserver/IMessageServer.java
+++ src/main/java/brown/communication/messageserver/IMessageServer.java
@@ -1,20 +1,33 @@
   package brown.communication.messageserver;
 
-import brown.communication.messages.IMessage;
+import brown.communication.messages.IInformationMessage;
+import brown.communication.messages.IInformationRequestMessage;
+import brown.communication.messages.IRegistrationMessage;
+import brown.communication.messages.ITradeMessage;
+import brown.communication.messages.ITradeRequestMessage;
 
+/**
+ * Interface for communication between server and agents. 
+ * @author andrewcoggins
+ *
+ */
 public interface IMessageServer {
   
   // server sends invitaions for registration
   public void openRegistration(); 
   
   // server receives registration message from agent. 
-  public void onRegistration(); 
+  public void onRegistration(IRegistrationMessage registrationMessage); 
   
   // server receives request for information from agent. 
-  public void onInformationRequest(); 
+  public void onInformationRequest(IInformationRequestMessage informationRequestMessage); 
   
   // server receives bid message from agent. 
-  public void onBid(IMessage bidMessage); 
+  public void onBid(ITradeMessage bidMessage); 
   
+  // server sends tradeRequest message
+  public void sendTradeRequest(ITradeRequestMessage tRequestMessage); 
   
+  // server sends information message
+  public void sendInfomationMessage(IInformationMessage informationMessage); 
 }
--- src/main/java/brown/platform/managers/library/SimulationManager.java
+++ src/main/java/brown/platform/managers/library/SimulationManager.java
@@ -3,17 +3,21 @@ package brown.platform.managers.library;
 import java.util.LinkedList;
 import java.util.List;
 
+import brown.communication.messageserver.IMessageServer;
+import brown.communication.messageserver.library.MessageServer;
 import brown.logging.library.PlatformLogging;
 import brown.platform.managers.ISimulationManager;
 import brown.platform.managers.IWorldManager;
 import brown.platform.simulation.ISimulation;
 import brown.platform.simulation.library.Simulation;
+import brown.system.setup.library.SimpleSetup;
 
 public class SimulationManager implements ISimulationManager {
 
     private List<ISimulation> simulations;
     private List<Integer> numSimulationRuns;  
     private boolean lock;
+    private IMessageServer messageServer; 
 
     public SimulationManager() {
         this.simulations = new LinkedList<>();
@@ -33,13 +37,30 @@ public class SimulationManager implements ISimulationManager {
     public void lock() {
         this.lock = true;
     }
-
+    
+    
+    public void startMessageServer() {
+      this.messageServer = new MessageServer(2121, new SimpleSetup());
+    }
+    // handle information requests
+    
+    // handle trade messages
+    
     public void runSimulation(int waitTime, int numRuns) throws InterruptedException {
       Thread.sleep(waitTime * 1000);
+      // send registration, register agents
+      
+      // start up the message server
+      
       for (int i = 0; i < numRuns; i++) {
-        // what needs to happen here? 
-        
-        // 
+
+        // send valuations
+    	  
+    	// for simulmarket: markets
+    	
+    	// run simultaneous markets
+    	  
+    	// update accounts 
       }
     }
     
