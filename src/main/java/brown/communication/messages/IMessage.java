1f8f5881c0f2f83cd58d987f081353950ebee67f
AmyGreenwald <amy@brown.edu>
2019-03-11 08:10:51 +0900

    spacing

--- src/main/java/brown/communication/messages/IMessage.java
+++ src/main/java/brown/communication/messages/IMessage.java
@@ -2,16 +2,15 @@ package brown.communication.messages;
 
 /**
  * A message is used to communicate between the agent and the server
- *
  * @author acoggins
+ *
  */
 public interface IMessage {
 	
-	/**
-	 * Get message ID
-	 * @return ID
-	 */
-	public Integer getMessageID();
-	
+  /**
+   * Get message ID
+   * @return ID
+   */
+   public Integer getMessageID();
 	
 }
