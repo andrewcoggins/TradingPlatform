e3990adc3234ff4e1d0e3bd7a06586fb66e15323
AmyGreenwald <amy@brown.edu>
2019-06-03 07:52:50 +0900

    comments (tradeables to carts)

--- src/main/java/brown/auction/value/valuation/ISpecificValuation.java
+++ src/main/java/brown/auction/value/valuation/ISpecificValuation.java
@@ -3,17 +3,16 @@ package brown.auction.value.valuation;
 import brown.platform.item.ICart;
 
 /**
- * IValuation specifies valuations for ITradeables. 
+ * IValuation specifies valuations for ICarts. 
  * An IValuation will be specific to a single agent.
  * @author andrew
- *
  */
 public interface ISpecificValuation {
   
   /**
-   * This method returns a valuation for some ITradeable.
-   * @param tradeable an ITradeable.
-   * @return the value the agent has for the input tradeable.
+   * This method returns a valuation for some ICart.
+   * @param cart an ICart.
+   * @return the value the agent has for the input cart.
    */
   public Double getValuation(ICart cart);
  
