b6e6ac84d7f717ed1ea753bbf27fd63148d8b244
andrewcoggins <andrew.coggins@breakawaypartners.com>
2019-07-11 02:12:37 -0400

    runs trivially

--- src/main/java/brown/user/main/library/ValuationConfig.java
+++ src/main/java/brown/user/main/library/ValuationConfig.java
@@ -37,4 +37,57 @@ public class ValuationConfig implements IValuationConfig {
     return this.itemNames; 
   }
 
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((itemNames == null) ? 0 : itemNames.hashCode());
+    result = prime * result
+        + ((valDistribution == null) ? 0 : valDistribution.hashCode());
+    result = prime * result
+        + ((valuationCons == null) ? 0 : valuationCons.hashCode());
+    result = prime * result
+        + ((valuationParams == null) ? 0 : valuationParams.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    ValuationConfig other = (ValuationConfig) obj;
+    if (itemNames == null) {
+      if (other.itemNames != null)
+        return false;
+    } else if (!itemNames.equals(other.itemNames))
+      return false;
+    if (valDistribution == null) {
+      if (other.valDistribution != null)
+        return false;
+    } else if (!valDistribution.equals(other.valDistribution))
+      return false;
+    if (valuationCons == null) {
+      if (other.valuationCons != null)
+        return false;
+    } else if (!valuationCons.equals(other.valuationCons))
+      return false;
+    if (valuationParams == null) {
+      if (other.valuationParams != null)
+        return false;
+    } else if (!valuationParams.equals(other.valuationParams))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "ValuationConfig [valDistribution=" + valDistribution
+        + ", valuationCons=" + valuationCons + ", valuationParams="
+        + valuationParams + ", itemNames=" + itemNames + "]";
+  }
+  
 }
