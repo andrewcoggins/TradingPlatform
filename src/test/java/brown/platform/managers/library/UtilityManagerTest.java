54ac60f49d2a8e7bf773729441126fdd73186f13
andrewcoggins <andrew.coggins@breakawaypartners.com>
2019-09-14 11:16:40 -0400

    utility manager test

--- src/test/java/brown/platform/managers/library/UtilityManagerTest.java
+++ src/test/java/brown/platform/managers/library/UtilityManagerTest.java
@@ -9,7 +9,13 @@ import java.util.Map;
 
 import org.junit.Test;
 
+import brown.auction.value.distribution.IValuationDistribution;
+import brown.auction.value.distribution.library.AdditiveValuationDistribution;
+import brown.auction.value.generator.IValuationGenerator;
+import brown.auction.value.generator.library.NormalValGenerator;
 import brown.auction.value.valuation.IGeneralValuation;
+import brown.auction.value.valuation.ISpecificValuation;
+import brown.auction.value.valuation.library.GeneralValuation;
 import brown.platform.accounting.IAccount;
 import brown.platform.accounting.library.Account;
 import brown.platform.item.ICart;
@@ -36,7 +42,7 @@ public class UtilityManagerTest {
     Map<Integer, IGeneralValuation> valuations = new HashMap<Integer, IGeneralValuation>(); 
     
     List<IItem> agentItems = new LinkedList<IItem>(); 
-    agentItems.add(new Item("test", 0)); 
+    agentItems.add(new Item("test", 1)); ; 
     
     List<IItem> agentItemsTwo = new LinkedList<IItem>(); 
     agentItemsTwo.add(new Item("test", 1)); 
@@ -47,7 +53,42 @@ public class UtilityManagerTest {
     IAccount acctOne = new Account(0, 100.0, agentCart); 
     IAccount acctTwo = new Account(0, 100.0, agentCartTwo); 
     
+    accounts.put(0, acctOne); 
+    accounts.put(1, acctTwo); 
     
+    Map<List<IItem>, ISpecificValuation> valOne = new HashMap<List<IItem>, ISpecificValuation>(); 
+    Map<List<IItem>, ISpecificValuation> valTwo = new HashMap<List<IItem>, ISpecificValuation>();
+
+    valuations.put(0, new GeneralValuation(valOne)); 
+    valuations.put(0, new GeneralValuation(valTwo)); 
+    
+    List<IValuationGenerator> gen = new LinkedList<IValuationGenerator>(); 
+    List<Double> valParams = new LinkedList<Double>(); 
+    valParams.add(2.0); 
+    valParams.add(0.1); 
+    
+    gen.add(new NormalValGenerator(valParams)); 
+    IValuationDistribution valDist = new AdditiveValuationDistribution(new Cart(agentItems), gen); 
+    ISpecificValuation valuation = valDist.sample(); 
+    
+    valOne.put(agentItems, valuation); 
+    valTwo.put(agentItems, valuation); 
+    
+    IGeneralValuation genOne = new GeneralValuation(valOne); 
+    IGeneralValuation genTwo = new GeneralValuation(valTwo); 
+    
+    valuations.put(0, genOne); 
+    valuations.put(1, genTwo); 
+    
+    utilManager.updateUtility(accounts, valuations); 
+    
+    Map<Integer, List<Double>> agentRecords = utilManager.getAgentRecords(); 
+    
+    for (Integer id : agentRecords.keySet()) {
+      List<Double> utils = agentRecords.get(id); 
+      assertTrue(utils.size() == 1); 
+      assertTrue(utils.get(0) > 100.0); 
+    }
   }
   
 }
