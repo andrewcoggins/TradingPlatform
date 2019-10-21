eb019c7e20279f9515f594ced221c951d4fff8ee
Andrew Coggins <acoggins38@gmail.com>
2019-10-21 12:00:26 -0400

    Tac auction (#43)
    * tac auction looking good
    
    * remove text
    
    * tac auction runs but is boring
    
    * tac auction runs but is boring
    
    * not sure

---
+++ src/simulations/java/brown/simulations/TACSimulation.java
@@ -0,0 +1,46 @@
+package brown.simulations;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TACSimulation extends AbsUserSimulation {
+
+  public TACSimulation(List<String> agentClass, String inputJSON,
+      String outFile, boolean writeToFile) {
+    super(agentClass, inputJSON, outFile, writeToFile);
+  }
+
+  public void run() throws InterruptedException {
+
+    ServerRunnable sr = new ServerRunnable();
+    AgentRunnable ar = new AgentRunnable(agentClass.get(0), "solo");
+
+
+    Thread st = new Thread(sr);
+    Thread at = new Thread(ar);
+//    Thread atTwo = new Thread(ar);
+
+    st.start();
+    if (agentClass != null) {
+      at.start();
+//      atTwo.start(); 
+    }
+
+    while (true) {
+      if (!st.isAlive()) {
+        at.interrupt();
+        break;
+      }
+      Thread.sleep(1000);
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
+    List<String> agentList = new LinkedList<String>();
+    agentList.add("brown.user.agent.library.TACAgent");
+    TACSimulation tacSim = new TACSimulation(agentList,
+        "input_configs/TAC_auction.json", "outfile", false);
+    tacSim.run();
+  }
+
+}
